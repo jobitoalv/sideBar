540fd1b82a906d3c1f223607af96100e883dd555
Job Alvarez <jobitoalv@gmail.com>
2023-01-12 14:40:14 +1000

    creating home button

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,19 @@
-import React, { useContext } from 'react'
-import { FaBars } from 'react-icons/fa'
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from './context';
 
 const Home = () => {
-  return <main>
-    <button className='sidebar-toggle'>
-      <FaBars/>
-    </button>
-    <button className='btn'>
-      show Modal
-    </button>
-  </main>
-}
+  const { openSidebar, openModal } = useGlobalContext();
+  return (
+    <main>
+      <button onClick={openSidebar} className='sidebar-toggle'>
+        <FaBars />
+      </button>
+      <button onClick={openModal} className='btn'>
+        show modal
+      </button>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
--- src/Modal.js
+++ src/Modal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useGlobalContext } from './context';
 import { FaTimes } from 'react-icons/fa';
 const Modal = () => {
   const { isModalOpen, closeModal } = useGlobalContext();
