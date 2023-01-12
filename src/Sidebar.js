46213f1611dae5ba25f4b719e7ffbb914c2ca96f
Job Alvarez <jobitoalv@gmail.com>
2023-01-12 14:35:39 +1000

    create icon side bar link

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -14,6 +14,12 @@ const Sidebar = () => {
       <ul className='links'>
         {links.map((link)=> {
           const {id, url, text, icon} = link;
+          return <li key= {id}>
+            <a href={url}>
+              {icon}
+              {text}
+            </a>
+          </li>
         })}
       </ul>
   </aside>
